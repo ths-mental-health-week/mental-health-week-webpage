78914b449e0ab4e4001fe0454ac9714420148167
jcelik <julius.cc@hotmail.com>
2019-02-18 10:40:55 +0100

    Added styled components

---
+++ components/Page.js
@@ -0,0 +1,23 @@
+import { ThemeProvider } from 'styled-components'
+import Meta from './Meta'
+// import Header from './Header'
+
+const theme = {
+	colors: {
+		white: '#fff',
+		primary: '#67a2ce',
+		secondary: '#edc98a'
+	}
+}
+
+const Page = props => (
+	<ThemeProvider theme={theme}>
+		<div>
+			<Meta />
+			{/* <Header /> */}
+			<>{props.children}</>
+		</div>
+	</ThemeProvider>
+)
+
+export default Page
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,20 @@
-export default () => <h1>Hello world!</h1>
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	text-align: center;
+	width: 100vw;
+	height: 100vh;
+	padding: 50px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	box-sizing: border-box;
+`
+
+export default () => (
+	<Wrapper>
+		<h1>THS Mental Health Week</h1>
+		<h2>Coming soon</h2>
+	</Wrapper>
+)
