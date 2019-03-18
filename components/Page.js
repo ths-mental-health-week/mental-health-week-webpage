0def46f444b4be6c9f5deb42ba8036559397def3
Unknown <julius.cc@hotmail.com>
2019-03-18 06:34:42 +0100

    Add Navbar

--- components/Page.js
+++ components/Page.js
@@ -1,12 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import Meta from './Meta'
 import Footer from './Footer'
+import Navbar from './Navbar'
 // import Header from './Header'
 import theme from '../utils/theme'
 
 const Page = ({ children }) => (
 	<ThemeProvider theme={theme}>
 		<div>
+			<Navbar />
 			<Meta />
 			{/* <Header /> */}
 			<>{children}</>
