87d8e3532077984baad2f56b7afe779f08eccd68
Unknown <julius.cc@hotmail.com>
2019-03-18 06:34:27 +0100

    Moved theme to own file

--- components/Page.js
+++ components/Page.js
@@ -2,20 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import Meta from './Meta'
 import Footer from './Footer'
 // import Header from './Header'
-
-const theme = {
-	colors: {
-		white: '#fff',
-		black: '#000',
-		darkGrey: '#555',
-		lightGrey: '#bbb',
-		primary: '#F38D74',
-		primaryDark: '#B63E28',
-		secondary: '#273138',
-		background: '#F38D74',
-		selection: '#ffc4b7'
-	}
-}
+import theme from '../utils/theme'
 
 const Page = ({ children }) => (
 	<ThemeProvider theme={theme}>
