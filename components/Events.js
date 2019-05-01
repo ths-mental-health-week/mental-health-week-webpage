37ca8fc7ccac8e1c374b76a3f5e80cf3c7982029
Unknown <julius.cc@hotmail.com>
2019-05-01 21:06:14 +0200

    Fix git errors

--- components/Events.js
+++ components/Events.js
@@ -144,7 +144,7 @@ export default () => (
 		<Day>
 			<DayTitle>Wednesday</DayTitle>
 			<EventCard
-				imgSrc="/static/images/events/3%20-%20Annika%20Schu%CC%88llerqvist.png"
+				imgSrc="/static/images/events/3%20-%20Annika%20Schullerqvist.png"
 				imgAlt="Annika Schüllerqvist"
 				title="Breakfast Yoga with Annika Schüllerqvist"
 				date="Wednesday, May 15, 2019"
