b1109191d17d6e17ea354cfc98d7daf19df7678a
thsmentalhealthweek <56064161+thsmentalhealthweek@users.noreply.github.com>
2020-01-21 23:11:54 +0100

    Minor moving of recruitment text

--- pages/index.js
+++ pages/index.js
@@ -90,11 +90,7 @@ export default () => (
 		<Content>
 			<Container>
 				<Recruitment>
-					
-				</Recruitment>
-				
-				<Introduction>
-						<h2>We are now recruiting!</h2>
+					<h2>We are now recruiting!</h2>
 					<p>
 						Do you want to be a part of Mental Health Week 2020?
 						Join us and make a difference! 
@@ -102,7 +98,9 @@ export default () => (
 						<a href="https://forms.gle/78w1rqQZcdQ1dm3X9">Join Mental Health Week 2020!</a>
 						
 					</p>
+				</Recruitment>
 				<Divider />
+				<Introduction>
 					<h2>What is Mental Health Week?</h2>
 					<p>
 						We believe that solutions happen when we start sharing
